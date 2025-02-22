583c20e684268639fd56e24780149b9dbf9c8a41
Ajishi Singh <ajishisingh2@gmail.com>
2025-02-22 10:20:50 +0530

    Connected backend to MongoDB Atlas and implemented WebSocket functionality

--- task-management/frontend/src/app/tasks/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TaskPage({ params }: { params: { id: string } }) {
-  return (
-    <div className="p-4">
-      <h1>Task Details</h1>
-      <p>Task ID: {params.id}</p>
-    </div>
-  );
-} 
